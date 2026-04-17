f4aad2257712418834a4ad1e44c79ed374c3ddd2
Daniela Villamizr Mendoza <daniv13596@gmail.com>
2026-04-17 18:47:48 -0500

    archivos de app de calculadora

---
+++ app/calculador.py
@@ -0,0 +1,17 @@
+# app/calculadora.py
+
+AUTORES = "oalzatev@eafit.edu.co , maurreac@eafit.edu.co , jmgarzonv@eafit.edu.co , dvillamizm@eafit.edu.co"  # IMPORTANTE: Reemplaza con los usuarios de correo de EAFIT de los estudiantes que participaron en la entrega, separados por comas.
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        raise ZeroDivisionError("No se puede dividir por cero")
+    return a / b
