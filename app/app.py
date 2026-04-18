95a59c5150382abe93c2fd8565c4186551ba3a2b
Daniela Villamizr Mendoza <daniv13596@gmail.com>
2026-04-17 19:22:21 -0500

    format changes

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ from .calculadora import sumar, restar, multiplicar, dividir
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     resultado = None
@@ -30,5 +31,6 @@ def index():
 
     return render_template("index.html", resultado=resultado)
 
+
 if __name__ == "__main__":  # pragma: no cover
-    app.run(debug=True, port=5000, host='0.0.0.0') # Quita debug=True para producción
+    app.run(debug=True, port=5000, host="0.0.0.0")  # Quita debug=True para producción
--- app/calculadora.py
+++ app/calculadora.py
@@ -2,14 +2,18 @@
 
 AUTORES = "oalzatev@eafit.edu.co , maurreac@eafit.edu.co , jmgarzonv@eafit.edu.co , dvillamizm@eafit.edu.co"  # IMPORTANTE: Reemplaza con los usuarios de correo de EAFIT de los estudiantes que participaron en la entrega, separados por comas.
 
+
 def sumar(a, b):
     return a + b
+
 
 def restar(a, b):
     return a - b
 
+
 def multiplicar(a, b):
     return a * b
+
 
 def dividir(a, b):
     if b == 0:
